3ddee5335f257dde03df889dc0ad50e1ddaab8dd
Ebrahim_Moqbel <ebra72himuni@gmail.com>
2026-09-14 02:25:59 +0100

    created all episodes cards

--- script.js
+++ script.js
@@ -4,17 +4,45 @@ function setup() {
   makePageForEpisodes(allEpisodes);
 }
 
-function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-}
+
+
 // create episode code of the season and the episode number
-function makeCodeForEpisode(season, episodeNumber){
+function makeEpisodeCode(season, episodeNumber){
   season = String(season).padStart(2,"0");
   episodeNumber = String(episodeNumber).padStart(2,"0");
 
   const code = "S" + season +"E" + episodeNumber ;
   return code;
 } 
+// create episode card to display all the episodes 
+const template =document.getElementById("episode-card");
 
+// render the episodes by cloning the template in the html and populating it with data
+function createEpisodeCard(episode){
+  const card =template.content.cloneNode(true);
+  card.querySelector("h2").textContent = episode.name;
+
+  const episodeNumber =makeEpisodeCode(episode.season,episode.number);
+  card.querySelector("[data-episode-number]").textContent = episodeNumber;
+
+  const image = card.querySelector("img");
+  image.src = episode.image.medium;
+  image.alt = `Scene from ${episodeNumber}, ${episode.name}`;
+
+  card.querySelector("[data-summary]").innerHTML =episode.summary;
+
+  return card;
+}
+
+
+
+
+// create all cards
+function makePageForEpisodes(episodeList) {
+  const rootElem = document.getElementById("root");
+
+  const episodeCards = episodeList.map(createEpisodeCard);
+
+  rootElem.append(...episodeCards);
+}
 window.onload = setup;
