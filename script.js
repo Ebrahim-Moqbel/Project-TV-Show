ba3071c54667a7f2b7b154b56807cc320af58a34
Ebrahim_Moqbel <ebra72himuni@gmail.com>
2026-09-14 01:46:31 +0100

    added a code generator function for the episodes

--- script.js
+++ script.js
@@ -8,5 +8,13 @@ function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
 }
+// create episode code of the season and the episode number
+function makeCodeForEpisode(season, episodeNumber){
+  season = String(season).padStart(2,"0");
+  episodeNumber = String(episodeNumber).padStart(2,"0");
+
+  const code = "S" + season +"E" + episodeNumber ;
+  return code;
+} 
 
 window.onload = setup;
